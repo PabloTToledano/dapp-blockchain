9073dc5e6ce4da59c53aacc9ae497a13b694eac7
JavierPlata <javi.333.6@gmail.com>
2021-02-03 18:12:00 +0100

    Update utils.py

--- Poll_dapp-main/src/utils.py
+++ Poll_dapp-main/src/utils.py
@@ -2,6 +2,7 @@ from web3 import Web3, HTTPProvider
 
 SERVER_HOST = 'http://localhost:8545'
 web3_socket = Web3(HTTPProvider(SERVER_HOST))
+web3_socket.middleware_onion.inject(geth_poa_middleware, layer=0)
 
 def clean_error_message(message):
     return str(message).replace("execution reverted: VM Exception while processing transaction: revert ", "")
